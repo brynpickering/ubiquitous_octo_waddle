09adecb3cfb71caabe16a52aa71870e1e3753e17
Bryn Pickering <17178478+brynpickering@users.noreply.github.com>
2023-09-14 15:05:18 +0100

    random commit

--- ubiquitous_octo_waddle/ubiquitous_octo_waddle.py
+++ ubiquitous_octo_waddle/ubiquitous_octo_waddle.py
@@ -1,1 +1,11 @@
 """Main module."""
+from pathlib import Path
+
+
+def my_path(extra: str):
+    """My path
+
+    Args:
+        extra (str): extra to add to print
+    """
+    print(Path(__file__), extra)
